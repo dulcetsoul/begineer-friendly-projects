98aa88605ece7671e5e2a813799dcb6f81ba26c6
dulcetsoul_ <ggstuti1999@gmail.com>
2020-05-21 13:38:07 +0530

    Correct the year

--- Day of the week/main.js
+++ Day of the week/main.js
@@ -2,7 +2,7 @@ var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var month=["January","Febraury","March","April","May","June","July","August","September","October","November","December"];
 function update(){
 	var d = new Date();
-	mon.innerHTML = 'Date : ' + d.getDate() + ' '+ month[d.getMonth()]+ ' ' + d.getYear()+ '<br' ;
+	mon.innerHTML = 'Date : ' + d.getDate() + ' '+ month[d.getMonth()]+ ' ' + d.getFullYear()+ '<br' ;
 	daz.innerHTML = 'Day : ' + day[d.getDay()]+ '<br>';
 	hr.innerHTML = d.getHours() + "<br> Hours";
 	min.innerHTML = d.getMinutes()+ "<br> Minutes";
